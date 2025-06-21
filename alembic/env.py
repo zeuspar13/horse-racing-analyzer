d954bb1068a2c37a1cf0fe6ccbb9bef4ac5d20ea
Steve <steve@malmstrommedia.com.com>
2025-06-22 00:30:01 +0200

    Add Alembic config and env example

---
+++ alembic/env.py
@@ -0,0 +1,47 @@
+from logging.config import fileConfig
+import os
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+target_metadata = None
+
+
+def get_url():
+    return os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/horse_racing")
+
+
+def run_migrations_offline():
+    context.configure(url=get_url(), target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        {
+            "sqlalchemy.url": get_url(),
+        },
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
